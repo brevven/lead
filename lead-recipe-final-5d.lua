19f91fa5ded8279b17c3f6b5b30a80387a881171
Brevven <brevingame@gmail.com>
2022-02-21 17:15:55 -0800

    fix 5dim's

--- lead-recipe-final-5d.lua
+++ lead-recipe-final-5d.lua
@@ -6,85 +6,84 @@ if mods["5dim_core"] then
   data.raw.recipe["lead-plate"].subgroup = "plates-plates"
   data.raw.item["lead-ore"].subgroup = "plates-ore"
 
-  -- Industrial furnace
-  data:extend({
-    {
-      type = "recipe",
-      name = "lead-plate-industrial-ore",
-      category = "industrial-furnace",
-      subgroup = "plates-industrial-ore",
-      order = "ad[lead-plate]",
-      icon = "__bzlead__/graphics/icons/lead-plate.png",
-      icon_size = 64, icon_mipmaps = 3,
-      enabled = true,
-      energy_required = 65,
-      ingredients = {{"lead-ore", 85}},
-      result = util.me.lead_plate,
-      result_count = 100,
-    }
-  })
+  if mods["5dim_resources"] then
+    -- Industrial furnace
+    data:extend({
+      {
+        type = "recipe",
+        name = "lead-plate-industrial-ore",
+        category = "industrial-furnace",
+        subgroup = "plates-industrial-ore",
+        order = "ad[lead-plate]",
+        icon = "__bzlead__/graphics/icons/lead-plate.png",
+        icon_size = 64, icon_mipmaps = 3,
+        enabled = true,
+        energy_required = 65,
+        ingredients = {{"lead-ore", 85}},
+        result = util.me.lead_plate,
+        result_count = 100,
+      }
+    })
 
-  -- Lead dust
-  data:extend(
-    {
-        {
-            type = "item",
-            name = "lead-dust",
-            subgroup = "plates-dust",
-            order = "d[lead-plate]",
-            icon = "__bzlead__/graphics/icons/lead-powder.png",
-            icon_size = 64, icon_mipmaps = 3,
-            stack_size = 200
-        },
-        {
+    -- Lead dust
+    data:extend(
+      {
+          {
+              type = "item",
+              name = "lead-dust",
+              subgroup = "plates-dust",
+              order = "d[lead-plate]",
+              icon = "__bzlead__/graphics/icons/lead-powder.png",
+              icon_size = 64, icon_mipmaps = 3,
+              stack_size = 200
+          },
+          {
+              type = "recipe",
+              name = "lead-plate-dust",
+              icon = "__bzlead__/graphics/icons/lead-plate.png",
+              icon_size = 64, icon_mipmaps = 3,
+              subgroup = "plates-plates2",
+              order = "d[lead-plate]",
+              category = "smelting",
+              energy_required = 3.2,
+              enabled = true,
+              ingredients = {
+                  {"lead-dust", 1}
+              },
+              result = util.me.lead_plate
+          },
+          {
+              type = "recipe",
+              name = "lead-dust",
+              icon = "__bzlead__/graphics/icons/lead-powder.png",
+              icon_size = 64, icon_mipmaps = 3,
+              category = "mashering",
+              subgroup = "",
+              order = "d[lead-plate]",
+              energy_required = 3.2,
+              enabled = true,
+              ingredients = {
+                  {"lead-ore", 1}
+              },
+              results = {
+                {type="item", name = "lead-dust", amount=2},
+              }
+          },
+          {
             type = "recipe",
-            name = "lead-plate-dust",
+            name = "lead-plate-industrial-dust",
+            category = "industrial-furnace",
+            subgroup = "plates-industrial-dust",
+            order = "ad[lead-plate]",
             icon = "__bzlead__/graphics/icons/lead-plate.png",
             icon_size = 64, icon_mipmaps = 3,
-            subgroup = "plates-plates2",
-            order = "d[lead-plate]",
-            category = "smelting",
-            energy_required = 3.2,
             enabled = true,
-            ingredients = {
-                {"lead-dust", 1}
-            },
-            result = util.me.lead_plate
-        },
-        {
-            type = "recipe",
-            name = "lead-dust",
-            icon = "__bzlead__/graphics/icons/lead-powder.png",
-            icon_size = 64, icon_mipmaps = 3,
-            category = "mashering",
-            subgroup = "",
-            order = "d[lead-plate]",
-            energy_required = 3.2,
-            enabled = true,
-            ingredients = {
-                {"lead-ore", 1}
-            },
-            results = {
-              {type="item", name = "lead-dust", amount=2},
-            }
-        },
-        {
-          type = "recipe",
-          name = "lead-plate-industrial-dust",
-          category = "industrial-furnace",
-          subgroup = "plates-industrial-dust",
-          order = "ad[lead-plate]",
-          icon = "__bzlead__/graphics/icons/lead-plate.png",
-          icon_size = 64, icon_mipmaps = 3,
-          enabled = true,
-          energy_required = 65,
-          ingredients = {{"lead-dust", 85}},
-          result = util.me.lead_plate,
-          result_count = 100,
-        }
-  })
-
-  if mods["5dim_resources"] then
+            energy_required = 65,
+            ingredients = {{"lead-dust", 85}},
+            result = util.me.lead_plate,
+            result_count = 100,
+          }
+    })
     local mks = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10"}
     local items = {"5d-masher-"}
     for i, mk in pairs(mks) do
@@ -92,7 +91,6 @@ if mods["5dim_core"] then
         util.add_ingredient(item..mk, "lead-plate", 2)
       end
     end
-
   end
 
   if mods["5dim_energy"] then
