9eb89522feb7e4d525c63ad140cde836f8cc7256
Brevven <brevingame@gmail.com>
2025-01-05 03:22:53 -0800

    hot metals defaults

--- compatibility/data/hot-metals.lua
+++ compatibility/data/hot-metals.lua
@@ -1,11 +1,11 @@
 local util = require("data-util");
 
 util.add_hot_metals({
-  {name="lead-plate", spoilTicks=20*60, icons={
+  {name="lead-plate", icons={
     { icon = "__bzlead__/graphics/icons/lead-plate.png", icon_size = 64},
     { icon = "__bzlead__/graphics/icons/lead-plate.png", icon_size = 64, tint={.9,.3,0, .5}},
   }},
-  {name="lead-expansion-bolt", spoilTicks=20*60, icons={
+  {name="lead-expansion-bolt", icons={
     { icon = "__bzlead__/graphics/icons/lead-expansion-bolt.png", icon_size = 128},
     { icon = "__bzlead__/graphics/icons/lead-expansion-bolt.png", icon_size = 128, tint={.9,.3,0, .5}},
   }},
